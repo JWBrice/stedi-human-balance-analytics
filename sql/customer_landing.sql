59fdfe015985c2bb3267f8d582e3fc5e880522bd
JWBrice <100400089+JWBrice@users.noreply.github.com>
2026-06-21 18:36:59 -0400

    Add customer landing Athena table script

---
+++ sql/customer_landing.sql
@@ -0,0 +1,14 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS stedi.customer_landing (
+  serialnumber string,
+  sharewithpublicasofdate bigint,
+  birthday string,
+  registrationdate bigint,
+  sharewithresearchasofdate bigint,
+  customername string,
+  email string,
+  lastupdatedate bigint,
+  phone string,
+  sharewithfriendsasofdate bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://stedi-jeanbrice-d609/customer/landing/';
